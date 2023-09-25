3f5dd6e1600b3761a1dbf4e348cd242022aa0c1b
Michal Dziok <michal@dziok.eu>
2023-09-25 19:03:15 +0200

    Create app for 3d printer notifications. Updated generated entities.

---
+++ apps/Ender/Ender.cs
@@ -0,0 +1,50 @@
+﻿using HomeAssistantGenerated;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomationsNetDaemon.apps.Ender
+{
+    [NetDaemonApp]
+    public class Ender
+    {
+        private readonly ILogger<Ender> _logger;
+        NotifyServices _services;
+        public Ender(IHaContext context, IScheduler scheduler, ILogger<Ender> logger, NotifyServices services)
+        {
+            _logger = logger;
+            _services = services;
+
+            Entities entities = new Entities(context);
+
+            entities.BinarySensor.OctoprintPrinting.StateChanges()
+                .WhenStateIsFor(e => e?.State == "off", TimeSpan.FromMinutes(15), scheduler)
+                .Subscribe(_ => TurnOffPrinter(entities));
+
+            entities.BinarySensor.OctoprintPrinting.StateChanges()
+                .Where(e => e.Old?.State == "on" && e.New?.State == "off")
+                .SubscribeSafe(_ => NotifyMichal(context), _logger);
+            
+
+        }
+
+        private void TurnOffPrinter(Entities entities)
+        {
+            _logger.LogInformation("Turning off printer after print.");
+            entities.Switch.PlugEnder.TurnOff();
+        }
+
+        private void NotifyMichal(IHaContext context)
+        {
+            _services.MobileAppMichalsIphone(
+                title: "Print Done!",
+                message: "Your fresh print is now ready.",
+                data: new { tag = "PrinterNotification" }
+            );
+        }
+    }
+}
