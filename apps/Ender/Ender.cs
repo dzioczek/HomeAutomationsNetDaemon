80503c86a88ac9621c4de8f823ece414d702d38e
Michał Dziok <michal@dziok.eu>
2024-09-04 11:43:55 +0200

    Updated netdaemon version. Updated entities,  updated phone name for notifications

--- apps/Ender/Ender.cs
+++ apps/Ender/Ender.cs
@@ -1,51 +1,42 @@
-﻿using HomeAssistantGenerated;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reactive;
-using System.Reactive.Concurrency;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reactive.Concurrency;
 
-namespace HomeAutomationsNetDaemon.apps.Ender
+namespace HomeAutomationsNetDaemon.apps.Ender;
+
+[NetDaemonApp]
+public class Ender
 {
-    [NetDaemonApp]
-    public class Ender
+    private readonly ILogger<Ender> _logger;
+    private readonly NotifyServices _services;
+
+    public Ender(IHaContext context, IScheduler scheduler, ILogger<Ender> logger, NotifyServices services)
     {
-        private readonly ILogger<Ender> _logger;
-        NotifyServices _services;
-        public Ender(IHaContext context, IScheduler scheduler, ILogger<Ender> logger, NotifyServices services)
-        {
-            _logger = logger;
-            _services = services;
+        _logger = logger;
+        _services = services;
 
-            Entities entities = new Entities(context);
+        Entities entities = new(context);
 
-            //entities.BinarySensor.OctoprintPrinting.StateChanges()
-            //    .WhenStateIsFor(e => e?.State == "off", TimeSpan.FromMinutes(15), scheduler)
-            //    .Subscribe(_ => TurnOffPrinter(entities));
+        //entities.BinarySensor.OctoprintPrinting.StateChanges()
+        //    .WhenStateIsFor(e => e?.State == "off", TimeSpan.FromMinutes(15), scheduler)
+        //    .Subscribe(_ => TurnOffPrinter(entities));
 
-            
-            entities.Sensor.Prusamk4.StateChanges()
-                .Where(e => e.Old?.State == "printing" && e.New?.State == "idle")
-                .SubscribeSafe(_ => NotifyMichal(context), _logger);
-            
 
-        }
+        entities.Sensor.Prusamk4.StateChanges()
+            .Where(e => e.Old?.State == "printing" && e.New?.State == "idle")
+            .SubscribeSafe(_ => NotifyMichal(), _logger);
+    }
 
-        private void TurnOffPrinter(Entities entities)
-        {
-            _logger.LogInformation("Turning off printer after print.");
-            entities.Switch.PlugEnder.TurnOff();
-        }
+    private void TurnOffPrinter(Entities entities)
+    {
+        _logger.LogInformation("Turning off printer after print");
+        entities.Switch.PlugEnder.TurnOff();
+    }
 
-        private void NotifyMichal(IHaContext context)
-        {
-            _services.MobileAppMichalsIphone(
-                title: "Print Done!",
-                message: "Your fresh print is now ready.",
-                data: new { tag = "PrinterNotification" }
-            );
-        }
+    private void NotifyMichal()
+    {
+        _services.MobileAppGreencell(
+            title: "Print Done!",
+            message: "Your fresh print is now ready.",
+            data: new { tag = "PrinterNotification" }
+        );
     }
 }
